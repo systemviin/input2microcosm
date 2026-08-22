e1cafff899f10bb5edac24fdc730744645aa7f5d
viin <viin@quantumtrip.org>
2026-08-22 18:15:42 -0400

    Getting started from template

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(input2microcosm::TemplateApp::new(cc)))),
     )
 }
 
